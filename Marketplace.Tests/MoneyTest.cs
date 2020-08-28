f4d43764b401c697cf540f1c53c199c8d3b5d0e2
Jurgen Stillaert <jurgen@stillaert.be>
2020-08-28 13:30:47 +0200

    Eerste aanzet modellen en valueobjects

---
+++ Marketplace.Tests/MoneyTest.cs
@@ -0,0 +1,17 @@
+﻿using Marketplace.Domain;
+using Xunit;
+
+namespace Marketplace.Tests
+{
+	public class MoneyTest
+	{
+		[Fact]
+		public void Money_objects_with_the_same_amount_shoud_be_equal()
+		{
+			var firstAmount = new Money(5);
+			var secondAmount = new Money(5);
+
+			Assert.Equal(firstAmount, secondAmount);
+		}
+	}
+}
